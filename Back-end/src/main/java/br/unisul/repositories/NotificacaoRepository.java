b03a1bc423c4a53cc69002084a52efaf2c64615a
Lucas <abreu.lucass@outlook.com>
2020-01-23 23:25:52 -0300

    finally

--- Back-end/src/main/java/br/unisul/repositories/NotificacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unisul.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.unisul.domain.Notificacao;
-
-public interface NotificacaoRepository extends JpaRepository<Notificacao, Integer> {
-
-}
